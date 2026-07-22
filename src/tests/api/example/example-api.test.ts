000295c0d0fdb6e41faad17736286954c27362dc
TqD39sy1 <c3pions@gmail.com>
2026-07-22 19:02:11 +0300

    Refactor API tests to remove optional chaining and update due dates in mock data

--- src/tests/api/example/example-api.test.ts
+++ src/tests/api/example/example-api.test.ts
@@ -6,29 +6,29 @@ import { assertResponseBody } from '@rest/utils/json-validator'
 test.describe('api: auth', () => {
   test('t_01_refresh_endpoint_returns_new_access_token', async ({ authApi }) => {
     const tokenResponse = await authApi.token(new AuthorizeRequest(credentials.username, credentials.password))
-    const refreshToken = tokenResponse.body?.data.authToken.refreshToken ?? ''
+    const refreshToken = tokenResponse.body.data.authToken.refreshToken ?? ''
 
     const response = await authApi.refresh(new RefreshTokenRequest(refreshToken))
     expect(response.status).toBe(200)
-    expect(response.body?.data.authToken.accessToken).toBeTruthy()
-    expect(response.body?.data.authToken.login).toBe(credentials.username)
-    expect(response.body?.data.authToken.accessTokenExpiresIn).toBe(credentials.accessTokenExpiresIn)
+    expect(response.body.data.authToken.accessToken).toBeTruthy()
+    expect(response.body.data.authToken.login).toBe(credentials.username)
+    expect(response.body.data.authToken.accessTokenExpiresIn).toBe(credentials.accessTokenExpiresIn)
   })
 
   test('t_02_get_activities_returns_list', async ({ activitiesApi }) => {
     const response = await activitiesApi.getActivities()
     expect(response.body).toBeInstanceOf(Array)
-    expect(response.body?.length).toBeGreaterThan(0)
-    expect(response.body?.[0].id).toBeTruthy()
-    expect(response.body?.[0].title).toBeTruthy()
-    expect(typeof response.body?.[0].completed).toBe('boolean')
+    expect(response.body.length).toBeGreaterThan(0)
+    expect(response.body[0].id).toBeTruthy()
+    expect(response.body[0].title).toBeTruthy()
+    expect(typeof response.body[0].completed).toBe('boolean')
     assertResponseBody(response.body, 'get-activities.json', ['dueDate'])
   })
 
   test('t_03_create_activity', async ({ activitiesApi }) => {
     const response = await activitiesApi.createActivity(createActivityRequest)
-    expect(response.body?.id).toBe(createActivityRequest.id)
-    expect(response.body?.title).toBe(createActivityRequest.title)
-    expect(response.body?.completed).toBe(createActivityRequest.completed)
+    expect(response.body.id).toBe(createActivityRequest.id)
+    expect(response.body.title).toBe(createActivityRequest.title)
+    expect(response.body.completed).toBe(createActivityRequest.completed)
   })
 })
